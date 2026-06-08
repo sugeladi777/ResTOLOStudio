0dd28983f556b1c45004add42c518fa93e175f67
Walnut <lck22@mails.tsinghua.edu.cn>
2026-06-08 12:00:48 +0800

    整理采集与结果面板结构

--- app/windows/studio_panels.py
+++ app/windows/studio_panels.py
@@ -5,39 +5,47 @@ from PyQt5.QtWidgets import QListWidget, QTextEdit, QVBoxLayout, QWidget
 
 class StudioPanelsMixin:
     def _build_studio_tabs(self):
-        acquisition_tab = QWidget()
-        acquisition_layout = QVBoxLayout(acquisition_tab)
-        acquisition_layout.addWidget(self._build_connection_group())
-        acquisition_layout.addWidget(self._build_scan_group())
-        acquisition_layout.addWidget(self._build_workflow_group())
-        acquisition_layout.addStretch()
+        self.tab_widget.addTab(self._build_acquisition_tab(), "采集控制")
+        self.tab_widget.addTab(self._build_results_tab(), "结果中心")
 
-        results_tab = QWidget()
-        results_layout = QVBoxLayout(results_tab)
+    def _build_acquisition_tab(self):
+        tab = QWidget()
+        layout = QVBoxLayout(tab)
+        layout.addWidget(self._build_connection_group())
+        layout.addWidget(self._build_scan_group())
+        layout.addWidget(self._build_workflow_group())
+        layout.addStretch()
+        return tab
+
+    def _build_results_tab(self):
+        tab = QWidget()
+        layout = QVBoxLayout(tab)
+
         self.session_list = QListWidget()
         self.result_list = QListWidget()
         self.result_detail_text = QTextEdit()
         self.result_detail_text.setReadOnly(True)
+
         open_scan_btn = self.create_button("使用选中结果做推理", self.use_selected_result_for_inference)
         reload_btn = self.create_button("刷新会话", self.reload_sessions)
-        results_layout.addWidget(self.create_label("实验会话"))
-        results_layout.addWidget(self.session_list)
-        results_layout.addWidget(self.create_label("扫描结果"))
-        results_layout.addWidget(self.result_list)
-        results_layout.addWidget(open_scan_btn)
-        results_layout.addWidget(reload_btn)
-        results_layout.addWidget(self.create_label("结果详情"))
-        results_layout.addWidget(self.result_detail_text)
+
+        layout.addWidget(self.create_label("实验会话"))
+        layout.addWidget(self.session_list)
+        layout.addWidget(self.create_label("扫描结果"))
+        layout.addWidget(self.result_list)
+        layout.addWidget(open_scan_btn)
+        layout.addWidget(reload_btn)
+        layout.addWidget(self.create_label("结果详情"))
+        layout.addWidget(self.result_detail_text)
 
         self.session_list.currentRowChanged.connect(self._session_selected)
         self.result_list.currentRowChanged.connect(self._result_selected)
-
-        self.tab_widget.addTab(acquisition_tab, "采集控制")
-        self.tab_widget.addTab(results_tab, "结果中心")
+        return tab
 
     def _build_connection_group(self):
         group = self.create_group("Nanonis 连接")
         layout = group.layout()
+
         self.nano_ip_edit = self.create_line_edit("127.0.0.1")
         self.nano_ip_edit.setText("127.0.0.1")
         self.nano_port_edit = self.create_line_edit("6501")
@@ -46,6 +54,7 @@ class StudioPanelsMixin:
         self.nano_version_edit.setText("10380")
         self.nano_status_text = QTextEdit()
         self.nano_status_text.setReadOnly(True)
+
         layout.addWidget(self.create_label("IP"))
         layout.addWidget(self.nano_ip_edit)
         layout.addWidget(self.create_label("端口"))
@@ -61,6 +70,7 @@ class StudioPanelsMixin:
     def _build_scan_group(self):
         group = self.create_group("扫描采集")
         layout = group.layout()
+
         self.scan_label_edit = self.create_line_edit("session_label")
         self.scan_bias_edit = self.create_line_edit("2.0")
         self.scan_bias_edit.setText("2.0")
@@ -73,6 +83,7 @@ class StudioPanelsMixin:
         self.scan_pixels_edit = self.create_line_edit("256")
         self.scan_pixels_edit.setText("256")
         self.scan_channels_edit = self.create_line_edit("Z (m), Current (A)")
+
         layout.addWidget(self.create_label("会话标签"))
         layout.addWidget(self.scan_label_edit)
         layout.addWidget(self.create_label("Bias (V)"))
@@ -98,10 +109,12 @@ class StudioPanelsMixin:
     def _build_workflow_group(self):
         group = self.create_group("半自动实验流")
         layout = group.layout()
+
         self.pulse_bias_edit = self.create_line_edit("2.5")
         self.pulse_bias_edit.setText("2.5")
         self.pulse_width_edit = self.create_line_edit("0.1")
         self.pulse_width_edit.setText("0.1")
+
         layout.addWidget(self.create_label("Pulse Bias (V)"))
         layout.addWidget(self.pulse_bias_edit)
         layout.addWidget(self.create_label("Pulse 宽度 (s)"))
